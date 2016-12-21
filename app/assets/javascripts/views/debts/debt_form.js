4a34c28be9614e06227650d8b4ef73a3ae5053e5
Travis Crampton <crampton.travis@gmail.com>
2016-12-21 10:33:28 -0800

    get off modified

--- app/assets/javascripts/views/debts/debt_form.js
+++ app/assets/javascripts/views/debts/debt_form.js
@@ -3,13 +3,15 @@ TheHiddenGreen.Views.DebtForm = Backbone.View.extend({
 	events:{
 		'submit form#debt_form' : 'submitDebtForm'
 	},
-	
+
 	initialize: function(){
 		this.render();
 	},
 
 	render: function(){
-		this.$el.html(JST['debts/debt_form']);
+		this.$el.html(JST['debts/debt_form']({
+			csrf_token: $("meta[name=csrf-token]")[0].content
+		}));
 	},
 
 	submitDebtForm: function(){
