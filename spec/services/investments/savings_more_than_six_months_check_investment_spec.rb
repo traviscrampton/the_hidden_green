13842a12bc2aee9649514856ab7843cc42f00748
Travis Crampton <crampton.travis@gmail.com>
2017-01-21 19:43:06 -0800

    another service object fully tested

--- spec/services/investments/savings_more_than_six_months_check_investment_spec.rb
+++ spec/services/investments/savings_more_than_six_months_check_investment_spec.rb
@@ -38,7 +38,37 @@ RSpec.describe Investments::SavingsMoreThanSixMonthsCheckInvestment do
 		end
 	end
 
-	
+	context "two savings accounts one investment account" do
+		let(:savings) { create(:account, a_type:'savings', amount:7000, interest_rate:0.08, name:'Bank Of America', accountable_id: month.id, accountable_type:month.class)}
+		let(:savings_2) { create(:account, a_type:'savings', amount:8000, interest_rate:0.04, name:'Grandmas Fund', accountable_id: month.id, accountable_type:month.class)}
+		let(:investment) { create(:investment, name:'S&B 500', amount: 1250, interest_rate: 0.07, investmentable_id: month.id, investmentable_type:month.class )}
 
+		before(:each) do
+			monthly_spending
+			savings
+			savings_2
+			investment
+			service.call
+			savings.reload
+			savings_2.reload
+			investment.reload
+		end
 
+		it "sets savings to equal six months spending" do
+			expect(month.savings).to eq month.six_months_spending
+		end
+
+		it "transfers the amount over to the investments" do
+			expect(investment.amount).to eq 4850.0
+		end
+
+		it "has the correct amount of advices" do
+			expect(month.advices.length).to eq 1
+		end
+
+		it "has the correct advices associated"  do
+			descriptions = month.advices.pluck(:description)
+			expect(descriptions).to include("Tranfer 3600.0 from your Grandmas Fund account to your S&B 500")
+		end
+	end
 end
