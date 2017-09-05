474ca979755d8bdb1e176ea9f85e2daf1c411035
Travis Crampton <crampton.travis@gmail.com>
2017-09-05 12:29:54 -0700

    updated what needed to be update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
 
   def after_sign_in_path_for(user)
-    new_account_path()
+    dashboard_path
   end
 
   def after_sign_out_path_for(resource)
